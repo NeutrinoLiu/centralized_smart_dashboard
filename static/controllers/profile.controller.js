d1f7b60b414c45adba324efc152d131734f7ebd2
Will <43940033+WillDaru22@users.noreply.github.com>
2020-10-02 16:11:59 -0500

    Add files via upload

---
+++ static/controllers/profile.controller.js
@@ -0,0 +1,26 @@
+// JavaScript source code
+(function() {
+	var app = angular.module('profile', []);
+	app.controller('profileController', ['$scope', '$window', 'http', profileController]);
+
+	function profileController($scope, $window, $http) {
+
+	}
+
+    //TODO Figure out how this even works
+
+	/*function readURL(input) {  // couldnt get this to work like I wanted it to
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }*/
+})();
