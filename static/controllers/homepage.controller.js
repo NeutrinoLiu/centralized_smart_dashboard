afe48679386e99674eec2af4f2a94f94c4a39c5f
Brenna <kinder2@wisc.edu>
2020-10-18 15:38:44 -0500

    "Coming Soon" and "Invalid" pages are complete but need to be tested. Homepage needs visual testing to continue. Having problems linking the stylesheet with angular.

--- static/controllers/homepage.controller.js
+++ static/controllers/homepage.controller.js
@@ -3,7 +3,8 @@
 	app.controller('homepageController', ['$scope', '$window', invalidPageController]);
 
 	function homepageController($scope, $window) {
-		
+		//TODO: Ask Sebastian how the scopes and functions should be formatted	
+
 		/*
 		These are the function that, when the buttons on the homepage are hit, will send to the various pages
 		*/
@@ -36,6 +37,12 @@
 		$scope.Maintenance = Maintenance;
 		function Maintenance() {
 			$window.location.href = "/Maintenance";
+		}
+
+		// Sending to Coming Soon
+		$scope.comingSoon = comingSoon;
+		function comingSoon() {
+			$window.location.href = "/comingSoon";
 		}
 
 	}
