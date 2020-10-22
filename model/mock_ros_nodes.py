8c7f2abbe59af69d20b0e10705aec6d24e8a1abc
Sebastian Piedra <piedra@wisc.edu>
2020-10-22 20:01:20 +0000

    Implemented ROS in docker and created skeleton for ROS code

---
+++ model/mock_ros_nodes.py
@@ -0,0 +1,35 @@
+import rospy
+
+'''
+Target topic list:
+/drive_cmd has motor info
+/nav_data has all navigation info
+
+TODO: Import NavigationMsg from WRobotics repo
+
+'''
+
+
+class NavData:
+
+    def __init__(self, node_name='dead_reckoning', topic_name='/nav_data', frequency=100):
+        rospy.init_node(node_name, anonymous=False)
+        self.navigation_pub = rospy.Publisher(topic_name, NavigationMsg, queue_size=1)
+        self.rate = rospy.Rate(frequency)
+
+    def talker(self):
+        ''''
+        Publishes fake navigation data using a NavigationMsg
+        TODO: implement
+        '''
+        msg = NavigationMsg()
+        self.navigation_pub.publsh(msg)
+        self.rate.sleep()
+
+
+class DriveData:
+
+    def __init__(self):
+        pass
+
+        
