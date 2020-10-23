e319de24003567d63a3695f8ffe7710992d5baef
Benbunnies <dirstineb@google.com>
2020-10-23 15:13:07 -0700

    changed error handler to coming soon page

--- routes.py
+++ routes.py
@@ -1,14 +1,44 @@
 from flask import render_template, json, abort
 
-def login():
-    return render_template('login.html')
 
-#TODO: might be useful to add server-side error handlers
-def error_handler( error ):
-    return render_template('invalid-page.html')
+# Render templates
+def home():
+    return render_template('homepage.html')
+
+
+def ai_navigation():
+    return render_template('AINavigation.html')
+
+
+def equipment_servicing():
+    return render_template("EquipmentServicing.html")
+
+
+def erdm():
+    return render_template("ERDM.html")
+
+
+def maintenance():
+    return render_template("Maintenance.html")
+
+
+def science():
+    return render_template("Science.html")
+
+
+# TODO: might be useful to add server-side error handlers
+def error_handler(error):
+    return render_template('ComingSoon.html')
 
 
 def init_website_routes(app):
     if app:
-        app.add_url_rule('/login', 'login', login, methods=['GET'])
+        app.add_url_rule('/', 'home', home, methods=['GET'])
+        app.add_url_rule('/home', 'home', home, methods=['GET'])
+        app.add_url_rule('/ai-navigation', 'ai_navigation', ai_navigation, methods=['GET'])
+        app.add_url_rule('/equipment-servicing', 'equipment_servicing', equipment_servicing, methods=['GET'])
+        app.add_url_rule('/erdm', 'erdm', erdm, methods=['GET'])
+        app.add_url_rule('/maintenance', 'maintenance',  maintenance, methods=['GET'])
+        app.add_url_rule('/science', 'science', science, methods=['GET'])
+
         app.register_error_handler(404, error_handler)
