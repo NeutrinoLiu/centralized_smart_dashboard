29e7b0d601d4f9988806dbca4f6ae5838f300237
Sebastian Piedra <piedra@wisc.edu>
2020-10-24 14:55:25 -0500

    Skeleton code for ui ROS nodes

---
+++ model/src/centralized_dashboard/src/ui_nodes.py
@@ -0,0 +1,33 @@
+import rospy
+
+
+class Subscriber:
+
+	def __init__(self):
+		pass
+
+	def get_heading(self):
+		pass
+
+	def get_target_coordinates(self):
+		pass
+
+	def get_current_coordinates(self):
+		pass
+
+	def get_motor_speed(self, motor_nums=[]):
+		pass
+
+	def get_all_nav_data(self):
+		pass
+
+class Publisher:
+
+	def __init__(sef):
+		pass
+
+	def set_target_coordinates(self):
+		pass
+
+	def set_motor_power(sef, motor_nums[], speeds=[]):
+		pass
