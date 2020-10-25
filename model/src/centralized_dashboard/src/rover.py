f7d088ca5e65f30c897cb60f066c97cfcc607059
liubangya <liubangya@gmail.com>
2020-10-24 23:45:01 -0500

    provide clear api to frontend

--- model/src/centralized_dashboard/src/rover.py
+++ model/src/centralized_dashboard/src/rover.py
@@ -6,16 +6,13 @@ class PosState:
     def __init__(self, lati, longt):
         self.lati = lati
         self.longt = longt
-        self.visited = False
-    def arrive():
-        self.visited = True
 
 class Rover:    # the object sending from local subscriber to the frontend
     def __init__(self, name): # just init the fields
         self.route_state = []
         self.gps_longt = 0.0
         self.gps_latit = 0.0
-        self.ori = 0
+        self.ori = 0.0
         self.speed = []
         self.arm = []
         self.remark = ''
@@ -28,7 +25,7 @@ class Cmd:      # the object sending from frontend to the local publisher
     def __init__(self,  new_route=[], 
                         new_arm=[], 
                         new_speed=[],
-                        remark='')
+                        remark='<unclaimed>')
     self.new_route = new_route
     self.new_arm = new_arm
     self.new_speed = new_speed
@@ -43,9 +40,6 @@ class Cmd:      # the object sending from frontend to the local publisher
         self.cmd_code = self.cmd_code | 0b0100  # 3rd bit: 
 
     def __repr__(self):
-        if remark == '' :
-            return "cmd purpose unclaimed"
-        else:
-            return "this cmd aims to " + remark
+        return "this cmd aims to " + remark
 
 
