42c64d6d0f6de1a4c8e5d8d3c52229ae27ad736e
liubangya <liubangya@gmail.com>
2020-10-30 23:08:59 -0500

    disable the flask reloader, or it will lead to multiple init of same name nodes

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 if __name__ == "__main__":
     # TODO: add route initializations here, both for templates and backend
     init_website_routes(app)
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True,use_reloader=False, host='0.0.0.0')
