53a0dd46debf7977c56dd3946abe53560020a2fc
Will <43940033+WillDaru22@users.noreply.github.com>
2020-11-11 01:18:52 -0600

    Added more unit test skeletons to the AI Navigation unit tests

--- static/testing/AINavigation.test.js
+++ static/testing/AINavigation.test.js
@@ -1,1 +1,95 @@
 // Unit testing file for the AI Navigation controller
+
+test('test homepage called', () => {
+
+})
+
+test('test init called', () => {
+
+})
+
+describe('Testing fullWaypoint', () => {
+	test('test full waypoint returns expected value', () => {
+
+	})
+
+	test('test full waypoint with negative numbers', () => {
+
+	})
+
+	test('test full waypoint with null input', () => {
+
+	})
+
+	test('test full waypoint with large numbers', () => {
+
+	})
+
+	test('test full waypoint with very small numbers', () => {
+
+	})
+
+	test('Estop button called', () => {
+
+	})
+
+	test('Go button called', () => {
+
+	})
+})
+
+describe('Testing lat2y', () => {
+	test('test lat2y', () => {
+
+	})
+})
+
+describe('Testing lat2x', () => {
+	test('test lat2x', () => {
+		
+	})
+})
+
+describe('Testing coordToXY', () => {
+	test('test with regular input', () => {
+
+	})
+
+	test('test with null input', () => {
+
+	})
+
+	test('test with negative input', () => {
+
+	})
+
+	test('test with very large input', () => {
+
+	})
+
+	test('test with very small input', () => {
+
+	})
+
+	test('test with same input', () => {
+
+	})
+
+	test('test with improper input', () => {
+
+	})
+})
+
+describe('Testing addWaypointToMap', () => {
+
+})
+
+describe('Testing waypointNew', () => {
+
+})
+
+describe('Testing deleteWaypoint', () => {
+	test('test with normal waypoint', () => {
+
+	})
+})
