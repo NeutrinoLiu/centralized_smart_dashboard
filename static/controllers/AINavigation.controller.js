9f8b97c13aa6dd6584e0e577733d36acbc8b3137
Sebastian Piedra Rodriguez <piedra@wisc.edu>
2020-11-12 19:49:48 +0000

    Integration of front end JS with backend

--- static/controllers/AINavigation.controller.js
+++ static/controllers/AINavigation.controller.js
@@ -40,30 +40,26 @@
         }
 
         function init() {
-            // TODO: sample response, delete when backend ready, points are camprandall and bascom
-
-            // 43.076163, -89.400227
-
-            // 43.069798, -89.412654
-            test_response = {
-                'data': {
-                    waypoints: [{'lat': 43.076163, 'long': -89.400227}, {'lat': 43.069798, 'long': -89.412654}],
-                    curr_coord: {'lat': 90.3456, 'long': -90.6543},
-                    notifications: ""
-                }
-            }
-
-            $http.get(PATH + '/api/waypoint')
-                .then ((response) => {
-                    response =test_response; // TODO: only for testing, delete when backend ready
+            $http.get(PATH + '/api/route')
+                .then((response) => {
                     for (index = 0; index < response.data.waypoints.length; index++) {  
                         $scope.waypoints.push(fullWaypoint(response.data.waypoints[index]));
                     }
+                }, (error) => {
+                    connectionLost()
+                });
 
-                    $scope.curr_coord.lat = response.data.curr_coord.lat;
-                    $scope.curr_coord.long = response.data.curr_coord.long;
+            $http.get(PATH + '/api/gps')
+                .then((response) => {
+                    $scope.curr_coord.lat = response.data.lat;
+                    $scope.curr_coord.long = response.data.long;
+                }, (error) => {
+                    connectionLost();
+                });
+
+            $http.get(PATH + '/api/notifications')
+                .then((response) => {
                     $scope.notifications = response.data.notifications;
-                    console.log($scope.waypoints);
                 }, (error) => {
                     connectionLost();
                 });
@@ -88,16 +84,12 @@
             return waypoint;
         }
 
-        // function lat2y(lat) { return Math.log(Math.tan((lat / 90 + 1) * PI_4 )) * RAD2DEG; }
-        // function lon2x(lon) { return lon; }
-
         function coordToXY(latitude, longitude) {
             /*
             Algorithm to change from coord to XY with the pixel scaling factor
             Scaling factor for latitude and longitude was manually calculated from pixel length
             and height of the map.
-            TODO: Take the difference from the current Latitude and longitude for the proper coordinates
-            // */
+            */
 
             scale_factor = 1;
             x_pos = Math.abs(parseInt(longitude) - longitude);
@@ -124,13 +116,10 @@
 
         function addWaypointToMap(waypoint) {
                 $scope.$apply();
-                console.log(waypoint);
                 top_ = waypoint['y_pos'].toString();
                 top_ = top_ + 'px';
                 left_ = waypoint['x_pos'].toString();
                 left_ = left_ + 'px';
-                console.log(document.getElementById(waypoint['index']))
-                console.log(document.getElementById('point-1'))
                 document.getElementById(waypoint['index']).style.position = 'absolute';
                 document.getElementById(waypoint['index']).style.top = top_;
                 document.getElementById(waypoint['index']).style.left = left_;
@@ -147,53 +136,35 @@
                 alert("Invalid coordinates for new waypoint");
             } else {
 
-                // TODO: sample response, delete when backend ready, points are camprandall and bascom
-                test_response = {
-                    'data': {
-                        'lat': latitude,
-                        'long': longitude
-                    }
-                }
-                $http.post( PATH + '/api/waypoint', 
+                $scope.waypoints.push(fullWaypoint({'lat': latitude, 'long': longitude}));
+                $http.post(PATH + '/api/route',
                     {
-                        'lat': latitude,
-                        'long': longitude
-                    }
-                )
-                .then( (response) => {
-                    response = test_response // TODO: remove after back end is ready
-                    // translate to XY and then amend
-                    $scope.waypoints.push(fullWaypoint(response.data));
-                    $scope.notifications += "New Waypoint Added" + '\n';
-                    console.log($scope.waypoints);
-                    console.log($scope.waypoints.length)
-                    addWaypointToMap($scope.waypoints[$scope.waypoints.length - 1]);
-                }, (error) => {
-                    connectionLost();
-                });
+                        'waypoints': $scope.waypoints
+                    } 
+                    ).then((response) => {
+                        $scope.waypoints = response.data.waypoints;
+                        $scope.notifications += "New Waypoint Added" + '\n'; // todo: add this through a http call
+                        addWaypointToMap($scope.waypoints[$scope.waypoints.length - 1]);
+                    }, (error) => {
+                        connectionLost();
+                    });
             }
         }
 
         // Removes the last waypoint added to our waypoints 
         function deleteLatestWaypoint() {
-            test_response = {
-                'data': {
-                    'success': true
-                }
-            }
-            $http.get(PATH + '/api/waypoint')
-            .then( (response) => {
-                response = test_response //TODO: remove after back end is done
-                if(response.data.success) {
-                    $scope.waypoints.pop();
-                    $scope.notifications += "Deleted Waypoint" + '\n';
-                }
-                else {
+            $scope.waypoints.pop();
+            $http.post(PATH + '/api/route',
+                {
+                    'waypoints': $scope.waypoints
+                } 
+                ).then((response) => {
+                    $scope.waypoints = response.data.waypoints;
+                    $scope.notifications += "Deleted Waypoint" + '\n'; // todo: add this through a http call
+                    addWaypointToMap($scope.waypoints[$scope.waypoints.length - 1]);
+                }, (error) => {
                     connectionLost();
-                }
-            }, (error) => {
-                connectionLost();
-            });
+                });
         }
 
         //Opens a new window with a live stream of the camera at the IP address sent
@@ -209,7 +180,8 @@
                 $http.get(PATH + '/api/go-button')
                     .then ((response) => {
                         if ( response.data.success) {
-                            // todo: change colors of waypoints, or something in map
+                            // TODO: change colors of waypoints, or something in map
+                            // TODO: add notigiation through http call 
                             $scope.notifications += "GO button pressed! Rover is moving." + '\n';
 
                         } else {
@@ -228,7 +200,12 @@
             alert("ESTOP PRESSED! Rover is force restarting.");
             $http.get(PATH + '/api/emergency-stop')
                 .then ((response) => {
-                    $scope.notifications += "ESTOP PRESSED! Rover is force restarting." + '\n';
+                    if (response.data.success) {
+                        // TODO: add notigiation through http call 
+                        $scope.notifications += "ESTOP PRESSED! Rover is force restarting." + '\n';
+                    } else {
+                        connectionLost();
+                    }
                 }, (error) => {
                     connectionLost();
                 });
