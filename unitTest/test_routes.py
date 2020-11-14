d9aa711ada9d4be1565c4aa1f87e95de1625f198
Benbunnies <dirstineb@google.com>
2020-11-13 19:20:36 -0800

    switch string comparison to dict comparison

--- unitTest/test_routes.py
+++ unitTest/test_routes.py
@@ -33,7 +33,7 @@ def test_gps1(client):
 def test_get_route_without_setting(client):
     url = "/api/route"
     response = client.get(url)
-    assert response.get_data() == (json.dumps({"success": False})+"\n").encode("UTF-8")
+    assert json.loads(response.get_data()) == {"success": False}
 
 def test_set_and_get_route(client):
     # check if setting works
@@ -41,38 +41,38 @@ def test_set_and_get_route(client):
     mock_request_data = {"waypoints": [{"lat": 0, "long": 0}, {"lat": -58.72, "long": 23.65}]}
     response = client.post(url, data=json.dumps(mock_request_data))
     # verify response is correct
-    assert response.get_data() == (json.dumps({"success": True, "waypoints": [{"lat": 0, "long": 0},
-                                                                               {"lat": -58.72, "long": 23.65}]})+"\n").encode("UTF-8")
+    assert json.loads(response.get_data()) == {"success": True, "waypoints": [{"lat": 0, "long": 0},
+                                                                               {"lat": -58.72, "long": 23.65}]}
     mock_correct_route = [GPSPoint(0, 0), GPSPoint(-58.72, 23.65)]
     # verify the route got changed correctly on the backend
     assert my_rover.route_state == mock_correct_route
     # verify get route now gets the correct route
     response = client.get(url)
-    assert response.get_data() == (json.dumps({"success": True, "waypoints": [{"lat": 0, "long": 0},
-                                                                               {"lat": -58.72, "long": 23.65}]})+"\n").encode("UTF-8")
+    assert json.loads(response.get_data()) == {"success": True, "waypoints": [{"lat": 0, "long": 0},
+                                                                               {"lat": -58.72, "long": 23.65}]}
     # checking if setting works if there is already a route
     mock_request_data = {"waypoints": [{"lat": 0, "long": 0}, {"lat": -58.72, "long": 23.65}, {"lat": -58, "long": 23},
                                        {"lat": -33.72, "long": 56.65}]}
     response = client.post(url, data=json.dumps(mock_request_data))
     # verify response is correct
-    assert response.get_data == (json.dumps({"success": True, "waypoints": [{"lat": 0, "long": 0},
+    assert json.loads(response.get_data) == {"success": True, "waypoints": [{"lat": 0, "long": 0},
                                                                               {"lat": -58.72, "long": 23.65},
                                                                               {"lat": -58, "long": 23},
-                                                                              {"lat": -33.72, "long": 56.65}]})+"\n").encode("UTF-8")
+                                                                              {"lat": -33.72, "long": 56.65}]}
     mock_correct_route2 = [GPSPoint(0, 0), GPSPoint(-58.72, 23.65), GPSPoint(-58, 23), GPSPoint(-33.72, 56.65)]
     # verify the route got changed correctly on the backend
     assert my_rover.route_state == mock_correct_route2
     response = client.get(url)
     # verify get route still gets the correct one
-    assert response.get_data == (json.dumps({"success": True, "waypoints": [{"lat": 0, "long": 0},
+    assert json.loads(response.get_data) == {"success": True, "waypoints": [{"lat": 0, "long": 0},
                                                                              {"lat": -58.72, "long": 23.65},
                                                                              {"lat": -58, "long": 23},
-                                                                             {"lat": -33.72, "long": 56.65}]})+"\n").encode("UTF-8")
+                                                                             {"lat": -33.72, "long": 56.65}]}
     # checking that clearing the set route works properly
     mock_request_data = {"waypoints": []}
     response = client.post(url, data=json.dumps(mock_request_data))
     # verify response is correct
-    assert response.get_data() == (json.dumps({"success": True, "waypoints": []})+"\n").encode("UTF-8")
+    assert json.loads(response.get_data()) == {"success": True, "waypoints": []}
     # verify route got removed on backend
     assert my_rover.route_state == []
 
@@ -81,13 +81,13 @@ def test_set_and_get_route(client):
 def test_emergency_stop(client):
     url = "/api/emergency-stop"
     response = client.get(url)
-    assert response.get_data() == (json.dumps({"success": True})+"\n").encode("UTF-8")
+    assert json.loads(response.get_data()) == {"success": True}
 
 def test_go_button_if_empty(client):
     url = "/api/go-button"
     response = client.get(url)
     # verify that the go button doesn't work if there is nothing to go to
-    assert response.get_data() == (json.dumps({"success": False})+"\n").encode("UTF-8")
+    assert json.loads(response.get_data()) == {"success": False}
 
 # TODO make a verification that the rover is actually moving towards the new target
 def test_go_button(client):
@@ -96,27 +96,27 @@ def test_go_button(client):
     response = client.post(url, data=json.dumps(mock_request_data))
     url = "/api/go-button"
     response = client.get(url)
-    assert response.get_data() == (json.dumps({"success": True})+"\n").encode("UTF-8")
+    assert json.loads(response.get_data()) == {"success": True}
 
 # check if empty notifications are empty
 def test_send_empty_notifications(client):
     url = "/api/notifications"
     response = client.get(url)
-    assert response.get_data() == (json.dumps({"success": True, "notifications": ""})+"\n").encode("UTF-8")
+    assert json.loads(response.get_data()) == {"success": True, "notifications": ""}
 
 #check if adding notifications works
 def test_add_notifications(client):
     url = "/api/notifications"
     mock_request_data = {"notifications": "\nnotification1\nnotification2"}
     response = client.post(url, data=json.dumps(mock_request_data))
-    assert response.get_data() == (json.dumps({"success": True, "\nnotifications": "notification1\nnotification2"})+"\n").encode("UTF-8")
+    assert json.loads(response.get_data()) == {"success": True, "\nnotifications": "notification1\nnotification2"}
     mock_request_data = {"notifications": "\nnotification3\nnotification4"}
     response = client.post(url, data=json.dumps(mock_request_data))
-    assert response.get_data() == (json.dumps({"success": True, "notifications":
-        "\nnotification1\nnotification2\nnotification3\nnotification4"})+"\n").encode("UTF-8")
+    assert json.loads(response.get_data()) == {"success": True, "notifications":
+        "\nnotification1\nnotification2\nnotification3\nnotification4"}
     response = client.get(url)
-    assert response.get_data() == (json.dumps({"success": True, "notifications":
-        "\nnotification1\nnotification2\nnotification3\nnotification4"})+"\n").encode("UTF-8")
+    assert json.loads(response.get_data()) == {"success": True, "notifications":
+        "\nnotification1\nnotification2\nnotification3\nnotification4"}
 
 # check to make sure get format route works
 def test_get_format_route(client):
