46f4099601ed3cff3cb1027709dcc22cc7a8eda1
Will <43940033+WillDaru22@users.noreply.github.com>
2020-11-23 16:40:59 -0600

    Removed depreciated pages from our project.  Updated some of the Cameras.HTML to reflect the new plans for the page.  Still more to do.  Started work on the Cameras JavaScript controller.

---
+++ static/controllers/Cameras.controller.js
@@ -0,0 +1,43 @@
+(function() {
+	var app = angular.module('Cameras', []);
+	app.controller('CamerasController', ['$scope', '$window', EquipmentServicingController]);
+
+	function CamerasController($scope, $window) {
+        $scope.homepage = homepage;
+        $scope.removeLatestCamera = removeLatestCamera;
+
+        $scope.cameraIPs = [];  // list of our camera IPs to store
+
+        function homepage() {  // takes user back to homepage
+            $window.location.href = "/home";
+        }
+
+	    // Removes the last camera IP added to our list
+        function removeLatestCamera() {
+            if ($scope.cameraIPs.length != 0) {
+                $scope.cameraIPs.pop();
+                $http.post(PATH + '/api/route',
+                    {
+                        'CameraIPs': $scope.cameraIPs
+                    } 
+                    ).then((response) => {
+                        if (response.data.success) {
+                            $scope.cameraIPs = []
+                            for (index = 0; index < response.data.cameraIPs.length; index++) {  
+                                $scope.cameraIPs.push(fullWaypoint(response.data.cameraIPs[index]));
+                            }
+
+                            addNotification("Removed Camera IP");
+                        } else {
+                            connectionLost();
+                        }
+                    }, (error) => {
+                        connectionLost();
+                    });
+            } else {
+                alert("No IP added. So we cannot remove anything");
+            }
+        }
+	}
+
+	})();
--- static/controllers/EquipmentServicing.controller.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	var app = angular.module('EquipmentServicing', []);
-	app.controller('EquipmentServicingController', ['$scope', '$window', EquipmentServicingController]);
-
-	function EquipmentServicingController($scope, $window) {
-
-	}
-
-	})();
