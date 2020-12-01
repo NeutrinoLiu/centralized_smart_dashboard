a0917a627c1589cb7d2e96c3abca8a9839f3903b
Brenna <kinder2@wisc.edu>
2020-11-30 20:15:01 -0600

    Added eStopButton to Cameras controller

--- static/controllers/Cameras.controller.js
+++ static/controllers/Cameras.controller.js
@@ -5,7 +5,7 @@
 	function CamerasController($scope, $window) {
         $scope.homepage = homepage;
         $scope.removeLatestCamera = removeLatestCamera;
-
+        $scope.eStopButton = eStopButton;
         $scope.cameraIPs = [];  // list of our camera IPs to store
 
         function homepage() {  // takes user back to homepage
@@ -41,6 +41,22 @@
 
         function addIP() {  // function to have us add a new IP address for the new camera
 
+        }
+
+        // Sends a force stop command to the rover
+        function eStopButton() {
+            alert("ESTOP PRESSED! Rover is force restarting.");
+            $http.get(PATH + '/api/emergency-stop')
+                .then((response) => {
+                    if (response.data.success) {
+                        // TODO: add notification through http call 
+                        addNotification("ESTOP PRESSED! Rover is force restarting.");
+                    } else {
+                        connectionLost();
+                    }
+                }, (error) => {
+                    connectionLost();
+                });
         }
 	}
 
